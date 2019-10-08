690e07fd476e42334c5c902675c4fe107ceb5032
yangjie <yangjie@localhost.localdomain>
2019-10-08 14:12:42 +0800

    1_class

---
+++ 1_class/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int MYSum()
+{
+   int i = 1;
+   int sum = 0;
+   for(;i <= 10 ;i++)
+   {
+      sum += i;
+   }
+   return sum; 
+}
+int main()
+{
+   int sum = MYSum();
+   printf("sum = %d\n",sum);
+   return 0;
+}
